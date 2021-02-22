30071251fd170bd1b2ece8e3148b1a4e351ba045
Abe Hidek <hidek.abe@outlook.com>
2021-02-22 11:13:26 -0300

    First Routes Creation

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,26 @@
-import express from 'express';
+import express, { request, response } from 'express';
 const app = express();
+
+/*
+ * GET => BUSCAR INFORMAÇÃO
+ * POST => SALVAR
+ * PUT => ALTERAR
+ * DELETE => DELETAR
+ * PATCH  => ALTERAÇÃO ESPECÍFICA
+ */
+
+// http>//localhost:3333/user
+app.get("/route1", (request, response)=> {
+    return response.send("HW NLW04");
+});
+
+app.get("/", (request,response)=>{
+    return response.json({ message: "HW NLW04" });
+});
+
+app.post("/", (request, response)=>{
+    // Recebeu os dados para salvar
+    return response.json({ message: "Os dados foram salvos com sucesso!" });
+});
 
 app.listen(3333, ()=> console.log('Server is Running!'));
