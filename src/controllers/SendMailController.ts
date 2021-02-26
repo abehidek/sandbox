4de926b30f15a7faec32f54b78d2bc1ce040a4e8
Abe Hidek <hidek.abe@outlook.com>
2021-02-26 11:25:11 -0300

    sending email functionality and relations between the SQLite Tables

--- src/controllers/SendMailController.ts
+++ src/controllers/SendMailController.ts
@@ -1,9 +1,13 @@
-import {Request, Response} from "express"
+import { Request, Response } from "express"
 import { getCustomRepository } from "typeorm";
+
+import { resolve } from 'path'
 
 import { SurveysRepository } from "../repositories/SurveysRepository";
 import { SurveysUsersRepository } from "../repositories/SurveysUsersRepository";
 import { UsersRepository } from "../repositories/UsersRepository";
+
+import SendMailService from "../services/SendMailService";
 
 
 
@@ -14,30 +18,52 @@ class SendMailController {
         const usersRepository = getCustomRepository(UsersRepository);
         const surveysRepository = getCustomRepository(SurveysRepository);
         const surveysUsersRepository = getCustomRepository(SurveysUsersRepository)
+        
 
-        const userAlreadyExists = await usersRepository.findOne({email})
+        const user = await usersRepository.findOne({email})
 
-        if (!userAlreadyExists) {
+        if (!user) {
             return response.status(400).json({
                 error: "User does not exist",
             })
         }
-        const surveyAlreadyExist = await surveysRepository.findOne({id: survey_id})
-        if (!surveyAlreadyExist) {
+        const survey = await surveysRepository.findOne({id: survey_id})
+        if (!survey) {
             return response.status(400).json({
                 error: "Survey does not exist"
             })
         }
+        const variables = {
+            name: user.name,
+            title: survey.title,
+            description: survey.description,
+            user_id: user.id,
+            link: process.env.URL_MAIL
+        }
+        const npsPath = resolve(__dirname, "..","views","emails","npsMail.hbs")
+
+        // VERIFICA SE NÃO HÁ REGISTRO DE PESQUISA PARA AQUELE USUÁRIO
+        const surveyUserAlreadyExists = await surveysUsersRepository.findOne({
+            where: [{user_id: user.id}, {value: null}],
+            relations: ["user","survey"],
+        })
+        if (surveyUserAlreadyExists) {
+            await SendMailService.execute(email, survey.title, variables, npsPath)
+            return response.json(surveyUserAlreadyExists)
+        }
         // SALVAR INFO NA TABELA
         const surveyUser = surveysUsersRepository.create({
-            user_id: userAlreadyExists.id,
+            user_id: user.id,
             survey_id
         })
         await surveysUsersRepository.save(surveyUser)
 
+        // ENVIAR EMAIL
+        
+        await SendMailService.execute(email, survey.title, variables, npsPath)
+        
         return response.json(surveyUser)
     }
-
 }
 
 export { SendMailController };
--- src/models/SurveyUser.ts
+++ src/models/SurveyUser.ts
@@ -1,5 +1,7 @@
-import { Column, CreateDateColumn, Entity, PrimaryColumn } from "typeorm";
+import { Column, CreateDateColumn, Entity, JoinColumn, ManyToOne, PrimaryColumn } from "typeorm";
 import { v4 as uuid } from "uuid";
+import { Survey } from "./Survey";
+import { User } from "./User";
 
 @Entity("surveys_users")
 
@@ -10,9 +12,17 @@ class SurveyUser {
     @Column()
     user_id: string;
     
+    @ManyToOne(()=>User)
+    @JoinColumn({name: "user_id"})
+    user: User
+
     @Column()
     survey_id: string;
     
+    @ManyToOne(()=> Survey)
+    @JoinColumn({name: "survey_id"})
+    survey: Survey
+
     @Column()
     value: number;
 
