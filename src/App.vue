05aca6668d1a2a702b7dbe7ba185f275f77ee4fe
abehidek <hidek.abe@outlook.com>
2021-03-01 16:29:11 -0300

    minor changes

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,18 @@
 <template>
+<div>
+
+  <div class="m-5">
+    <p class="flex justify-center text-xl lg:text-3xl lg:justify-start">Here some cards for you:</p>
+  </div>
+
   <div class="justify-center grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
     <Card/>
-    <Card/>
-    <Card/>
-    <Card/>
-    <Card/>
   </div>
+</div>
 </template>
 
 <script setup>
-import Card from './components/Card.vue'
-
+import Card from './components/Card.vue';
 </script>
 
 <style>
--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,7 +1,7 @@
 <template>
 <div class="w-100 bg-gray-200 rounded-md p-5 m-5">  
   <div class="mt-2 mb-5">
-    <p class="text-3xl mb-5 font-semibold">aaaa</p>
+    <p class="text-3xl mb-5 font-semibold">A big title</p>
     <p class="text-lg">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque laboriosam dolores molestiae officia? Dignissimos ad fugiat, dolores ipsam veniam inventore ipsa explicabo unde, quia nobis perferendis dolore dolorem, animi nihil?</p>
   </div>
   <button @click="state.count++" class="border-gray-700 border-2 p-2 rounded-md">COUNT IS: {{ state.count }}</button>
