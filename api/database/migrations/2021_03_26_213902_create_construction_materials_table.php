48ec921bdd6c3f3368232f4616c40c726d75eafe
Abe Hidek <hidek.abe@outlook.com>
2021-03-26 18:46:24 -0300

    Models and Migrations
    ConstructionMaterial modeland migration created and users and passwords migrations and models deleted

--- api/database/migrations/2021_03_26_213902_create_construction_materials_table.php
+++ api/database/migrations/2021_03_26_213902_create_construction_materials_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateConstructionMaterialsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
+        Schema::create('construction_materials', function (Blueprint $table) {
+            $table->increments('id');
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('desc');
+            $table->string('brand');
+            $table->string('qtd');
             $table->timestamps();
         });
     }
@@ -31,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('construction_materials');
     }
 }
