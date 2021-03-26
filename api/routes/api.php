81cdc965ae24365e95d63c0d21d660aa577814ab
Abe Hidek <hidek.abe@outlook.com>
2021-03-26 18:55:22 -0300

    Controllers and Routes
    Controllers and API routes created

--- api/routes/api.php
+++ api/routes/api.php
@@ -1,19 +1,4 @@
 <?php
+use App\Http\Controllers\ConstructionMaterialController;
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::apiResource('materials', ConstructionMaterialController::class);
