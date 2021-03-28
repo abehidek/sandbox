c4d9d316153803db983d34cc1f2ad3d843ef9ef5
Abe Hidek <hidek.abe@outlook.com>
2021-03-27 23:52:46 -0300

    Created public folder
    Added the PDF exam.

--- website/src/views/About.vue
+++ website/src/views/About.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about p-10">
+    <h1 class="flex justify-center mb-8 text-5xl">This is an about page</h1>
   </div>
 </template>
