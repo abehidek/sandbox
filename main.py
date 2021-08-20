69d126102e066600689f147552fd33c32bdeee93
Abe Hidek <hidek.abe@outlook.com>
2021-08-20 17:29:18 -0300

    Game over mechanics

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
-import random
 import pygame
+import random
+
 from pygame.locals import *
 
 
+# helper functions
 def on_grid_random():
     x = random.randint(0, 590)
     y = random.randint(0, 590)
     return x//10 * 10, y//10 * 10
 
 
-def collision (c1, c2):
+def collision(c1, c2):
     return (c1[0] == c2[0]) and (c1[1] == c2[1])
 
 
+# definition for snake movement
 UP = 0
 RIGHT = 1
 DOWN = 2
@@ -24,39 +27,60 @@ pygame.display.set_caption('Snake')
 
 snake = [(200, 200), (210, 200), (220, 200)]
 snake_skin = pygame.Surface((10, 10))
-snake_skin.fill((255, 255, 255))
+snake_skin.fill((255, 255, 255))  # filling snake skin with white color
 
 apple_pos = on_grid_random()
 apple = pygame.Surface((10, 10))
-apple.fill((255, 0, 0))
+apple.fill((255, 0, 0))  # filling apple skin with red color
 
 my_direction = LEFT
 
 clock = pygame.time.Clock()
 
-while True:
+font = pygame.font.Font('OpenSans-Bold.ttf', 18)
+
+score = 0
+game_over = False
+
+while not game_over:
     clock.tick(20)
     for event in pygame.event.get():
+
         if event.type == QUIT:
             pygame.quit()
             exit()
+
         if event.type == KEYDOWN:
-            if event.key == K_UP:
+            if event.key == K_UP and my_direction != DOWN:
                 my_direction = UP
-            if event.key == K_RIGHT:
+            if event.key == K_RIGHT and my_direction != LEFT:
                 my_direction = RIGHT
-            if event.key == K_DOWN:
+            if event.key == K_DOWN and my_direction != UP:
                 my_direction = DOWN
-            if event.key == K_LEFT:
+            if event.key == K_LEFT and my_direction != RIGHT:
                 my_direction = LEFT
 
     if collision(snake[0], apple_pos):
         apple_pos = on_grid_random()
         snake.append((0, 0))
+        score += 1
 
+    # Check if snake collided with boundaries
+    if snake[0][0] == 600 or snake[0][1] == 600 or snake[0][0] < 0 or snake[0][1] < 0:
+        game_over = True
+        break
+
+    # Check if the snake has hit itself
+    for i in range(1, len(snake) - 1):
+        if snake[0][0] == snake[i][0] and snake[0][1] == snake[i][1]:
+            game_over = True
+            break
+
+    # makes the body of the snake follows the head
     for i in range(len(snake) - 1, 0, -1):
         snake[i] = (snake[i - 1][0], snake[i - 1][1])
 
+    # changes the direction of the movement of the snake
     if my_direction == UP:
         snake[0] = (snake[0][0], snake[0][1] - 10)
     if my_direction == RIGHT:
@@ -67,7 +91,36 @@ while True:
         snake[0] = (snake[0][0] - 10, snake[0][1])
 
     screen.fill((0, 0, 0))
+    for x in range(0, 600, 10):  # Draw vertical lines
+        pygame.draw.line(screen, (40, 40, 40), (x, 0), (x, 600))
+    for y in range(0, 600, 10):  # Draw vertical lines
+        pygame.draw.line(screen, (40, 40, 40), (0, y), (600, y))
+
+    score_font = font.render(f'Score: {score}', True, (255, 255, 255))
+    score_rect = score_font.get_rect()
+    score_rect.topleft = (600 - 120, 10)
+    screen.blit(score_font, score_rect)
+
     screen.blit(apple, apple_pos)
+
     for pos in snake:
         screen.blit(snake_skin, pos)
+
+    if game_over:
+        break
+
     pygame.display.update()
+
+while True:
+    game_over_font = pygame.font.Font('OpenSans-Bold.ttf', 75)
+    game_over_screen = game_over_font.render('Game Over', True, (255, 255, 255))
+    game_over_rect = game_over_screen.get_rect()
+    game_over_rect.midtop = (600 / 2, 10)
+    screen.blit(game_over_screen, game_over_rect)
+    pygame.display.update()
+    pygame.time.wait(500)
+    while True:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                exit()
