006d716da55b052f1c36a3438de26a9bac86fb19
Abe Hidek <hidek.abe@outlook.com>
2021-08-20 16:38:26 -0300

    apple and snake created

--- main.py
+++ main.py
@@ -1,12 +1,35 @@
+import random
 import pygame
 from pygame.locals import *
+
+UP = 0
+RIGHT = 1
+DOWN = 2
+LEFT = 3
+
 
 pygame.init()
 screen = pygame.display.set_mode((600, 600))
 pygame.display.set_caption('Snake')
+
+snake = [(200, 200), (210, 200), (220, 200)]
+snake_skin = pygame.Surface((10, 10))
+snake_skin.fill((255, 255, 255))
+
+apple_pos = (random.randint(0, 590), random.randint(0, 590))
+apple = pygame.Surface((10, 10))
+apple.fill((255, 0, 0))
+
+
+my_direction = LEFT
+
 while True:
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             exit()
+    screen.fill((0, 0, 0))
+    screen.blit(apple, apple_pos)
+    for pos in snake:
+        screen.blit(snake_skin, pos)
     pygame.display.update()
