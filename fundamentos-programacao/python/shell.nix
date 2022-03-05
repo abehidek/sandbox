e8f5509a481bdff9d31f808f97cad82540888536
abehidek <hidek.abe@outlook.com>
2022-03-05 16:04:39 -0300

    configuração de ambiente virtual adicionado em python

---
+++ fundamentos-programacao/python/shell.nix
@@ -0,0 +1,22 @@
+# shell.nix
+# Esse arquivo é usado para preparar o ambiente de python em sistemas UNIX como o Linux e o OSX (da Apple), no entanto, para sistemas Windows, é necessário utilizar outras ferramentas como o venv
+{ pkgs ? import <nixpkgs> {} }:
+let
+  my-python = pkgs.python38;
+  python-with-my-packages = my-python.withPackages (p: with p; [
+    pygame
+    tkinter
+    pip
+    # other python packages you want
+  ]);
+in
+pkgs.mkShell {
+  buildInputs = [
+    python-with-my-packages
+    pkgs.pfetch
+  ];
+  shellHook = ''
+    PYTHONPATH=${python-with-my-packages}/${python-with-my-packages.sitePackages}
+    # maybe set more env-vars
+  '';
+}
