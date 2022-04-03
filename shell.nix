25f084d6b0d485869def9ca8231818d0497b71ca
abehidek <hidek.abe@outlook.com>
2022-04-02 23:12:23 -0300

    Setting up dev env

---
+++ shell.nix
@@ -0,0 +1,11 @@
+{ pkgs ? import <nixpkgs> {} }:
+let 
+      unstable = import (fetchTarball https://nixos.org/channels/nixos-unstable/nixexprs.tar.xz) { };
+in
+pkgs.mkShell {
+  buildInputs = [
+    unstable.nodejs
+    # keep this line if you use bash
+    pkgs.bashInteractive
+  ];
+}
