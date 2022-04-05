66f197863249c1e92a1faad0cd74182d29d43b57
abehidek <hidek.abe@outlook.com>
2022-04-04 21:47:29 -0300

    fixed linux shell bug on d01.py

--- fundamentos-programacao/python/02-atividades/d01_adivinhacao.py
+++ fundamentos-programacao/python/02-atividades/d01_adivinhacao.py
@@ -1,13 +1,15 @@
-from os import system
 from random import randint
 
+
 def generate_number():
-    rand_number = randint(1,101)
+    rand_number = randint(1, 101)
     return rand_number
+
 
 def guess():
     guess_number = int(input("Digite um número entre 1 e 100: "))
     return guess_number
+
 
 def check(rand_number, guess_number):
     if rand_number == guess_number:
@@ -15,33 +17,38 @@ def check(rand_number, guess_number):
     else:
         return False
 
+
 def game(attempts, life):
     rand_number = generate_number()
     win = False
-    while(attempts > 0 and life > 0):
-        print("Sua vida:",life,"\nSuas tentativas: ",attempts)
+    while attempts > 0 and life > 0:
+        print("Sua vida:", life, "\nSuas tentativas: ", attempts)
         guess_number = guess()
         if check(rand_number, guess_number):
             win = True
             break
         else:
-            attempts-=1
+            attempts -= 1
             life -= abs(rand_number - guess_number)
             print(f"Você tomou {abs(rand_number - guess_number)} de dano")
-            print("Tentativa errada", ('\nSua vida esgotou' if life <= 0 else "\nTente novamente"))
+            print(
+                "Tentativa errada",
+                ("\nSua vida esgotou" if life <= 0 else "\nTente novamente"),
+            )
             input("Aperte qualquer coisa para continuar")
-            
-    print('Você', ('venceu' if win else 'perdeu'))
+
+    print("Você", ("venceu" if win else "perdeu"))
+
 
 def main():
     print("Bem vindo ao jogo")
-    while(True):
+    while True:
         game(3, 50)
         opc = input("Deseja continuar?: S/N  ").upper()
-        if opc == 'N':
+        if opc == "N":
             break
         else:
             continue
 
-main()
 
+main()
