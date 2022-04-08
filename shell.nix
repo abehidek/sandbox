36779e567605c851bbcdb53432040bd6e4301b7e
abehidek <hidek.abe@outlook.com>
2022-04-07 21:25:18 -0300

    change python version to 3.10

--- shell.nix
+++ shell.nix
@@ -2,18 +2,16 @@
 { pkgs ? import <nixpkgs> {} }:
 let
   unstable = import (fetchTarball https://nixos.org/channels/nixos-unstable/nixexprs.tar.xz) { };
-  my-python = pkgs.python38;
+  my-python = pkgs.python310;
   python-with-my-packages = my-python.withPackages (p: with p; [
     pygame
-    tkinter
-    pip
+    tkinter 
     # other python packages you want
   ]);
 in
 pkgs.mkShell {
   buildInputs = [
     python-with-my-packages
-    pkgs.pfetch
     unstable.jdk
     pkgs.pfetch
   ];
