794535dd7d976421d1952960c90569c013c7c946
abehidek <hidek.abe@outlook.com>
2022-04-14 18:31:30 -0300

    Add typescript lang and lsp to shell env

--- shell.nix
+++ shell.nix
@@ -1,10 +1,13 @@
-{ pkgs ? import <nixpkgs> {} }:
+{ pkgs ? import <nixpkgs> { } }:
 let
-    unstable = import (fetchTarball https://nixos.org/channels/nixos-unstable/nixexprs.tar.xz) { };
-in
-pkgs.mkShell {
+  unstable = import
+    (fetchTarball "https://nixos.org/channels/nixos-unstable/nixexprs.tar.xz")
+    { };
+in pkgs.mkShell {
   buildInputs = [
     unstable.nodejs
+    unstable.typesript
+    unstable.typescript-language-server
     # keep this line if you use bash
     pkgs.bashInteractive
   ];
