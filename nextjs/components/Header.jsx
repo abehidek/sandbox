6c2dec8a5e1a560b02c2696b83c7d106fdd6b109
abehidek <hidek.abe@outlook.com>
2022-04-15 19:58:59 -0300

    Setup build config

--- nextjs/components/Header.jsx
+++ nextjs/components/Header.jsx
@@ -4,7 +4,7 @@ export default function Header() {
   return (
     <header>
       <div className="container">
-        <Link href="/">
+        <Link href="/" passHref>
           <h2>Dev Blog</h2>
         </Link>
       </div>
