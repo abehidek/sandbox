935f91324a00ee7392ce190fb06928ea04ce2c85
abehidek <hidek.abe@outlook.com>
2022-04-15 17:42:04 -0300

    Add home page functionality
    - Displaying front matter, image, title and date
    - Fetching data from github repo
    - Basic styling added

--- nextjs/pages/_app.js
+++ nextjs/pages/_app.js
@@ -1,7 +1,14 @@
 import '../styles/globals.css'
-
+import Header from '../components/Header'
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Header />
+      <main className='container'>
+        <Component {...pageProps} /> 
+      </main>
+    </>
+  )
 }
 
 export default MyApp
