e9737f12878240ee05e6b28c4e5d0b10d449d8a3
abehidek <hidek.abe@outlook.com>
2022-04-16 16:50:59 -0300

    Fix jsx-no-target-blank error

--- nextjs/components/Header.jsx
+++ nextjs/components/Header.jsx
@@ -11,10 +11,10 @@ export default function Header() {
         </h2>
         <div className="social-media">
           <h2>
-            <a target="_blank" href="https://github.com/abehidek" ><AiFillGithub /></a>
-            <a target="_blank" href="https://gitlab.com/abehidek" ><AiFillGitlab /></a>
-            <a target="_blank" href="mailto:hidek.abe@outlook.com" ><AiFillMail /></a>
-            <a target="_blank" href="https://twitter.com/abehidek" ><AiOutlineTwitter /></a>
+            <a rel="noreferrer" target="_blank" href="https://github.com/abehidek" ><AiFillGithub /></a>
+            <a rel="noreferrer" target="_blank" href="https://gitlab.com/abehidek" ><AiFillGitlab /></a>
+            <a rel="noreferrer" target="_blank" href="mailto:hidek.abe@outlook.com" ><AiFillMail /></a>
+            <a rel="noreferrer" target="_blank" href="https://twitter.com/abehidek" ><AiOutlineTwitter /></a>
           </h2>
         </div>
       </div>
