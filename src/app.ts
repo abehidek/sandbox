3b66bb9daeb4dd9cd43738451c2caf002dccda02
abehidek <hidek.abe@outlook.com>
2022-06-01 20:15:22 -0300

    Create router file

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,15 @@
 import express from 'express';
 import config from 'config';
 import connect from './utils/connect';
+import routes from './routes';
 
 const port = config.get<number>('port');
 const app = express();
 
-app.get("/", function (req, res) {
-  res.json({ hello: "World" })
-})
+app.use(express.json());
 
-app.listen(port, () => { console.log("Listening on port", port) })
+app.listen(port, () => {
+  console.log('Listening on port', port);
+
+  routes(app);
+});
