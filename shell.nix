c3dca9eee4fbb7153be691894749d5807dacd19c
abehidek <hidek.abe@outlook.com>
2022-06-03 20:02:38 -0300

    Add nix dev env

---
+++ shell.nix
@@ -0,0 +1,13 @@
+{ pkgs ? import <nixpkgs> {} }:
+let
+  unstable = import (fetchTarball "https://nixos.org/channels/nixos-unstable/nixexprs.tar.xz") { };
+in pkgs.mkShell {
+  buildInputs = [
+    unstable.nodejs
+    unstable.nodePackages.typescript
+    unstable.nodePackages.typescript-language-server
+
+    # keep this line if you use bash
+    pkgs.bashInteractive
+  ];
+}
