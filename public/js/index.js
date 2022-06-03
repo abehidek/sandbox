0866eb7b63438ba1941c7c29d7c1e8e23b7a5839
abehidek <hidek.abe@outlook.com>
2022-06-03 19:46:43 -0300

    Add delete message functionality

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,5 @@
-let username = window.prompt("Tell your name: ")
+// let username = window.prompt("Tell your name: ")
+let username = "abe123"
 
 if (username === "" || !username) {
     username = "anon";
@@ -13,19 +14,36 @@ const firebaseConfig = {
     messagingSenderId: "395186562591",
     appId: "1:395186562591:web:d0474fb24acc55e9ac572a"
 };
-  
+
 firebase.initializeApp(firebaseConfig);
 const db = firebase.database();
-
 const fetchChat = db.ref("messages/");
+
 fetchChat.on("child_added", function (snapshot) {
     const messages = snapshot.val();
-    const message = `<li class=${username === messages.usr ? "sent" : "receive"}>
-    <span>${messages.usr}: </span> ${messages.msg}
-    </li>`
+    const message = `
+        <li class=${username === messages.usr ? "sent" : "receive"}>
+            <span>${messages.usr}: </span> ${messages.msg}
+            <button id=${snapshot.key} class="deleteMessage">deletar</button>  
+        </li>`
+
     document.querySelector("#messages").innerHTML += message;
+    arrMsgs = Array.from(document.querySelectorAll(".deleteMessage"));
+    arrMsgs.forEach((msg) => {
+        msg.addEventListener('click', () => {
+            deleteMessage(msg.id);
+        });
+    })
+
     document.querySelector("#chat").scrollTop = document.querySelector("#chat").scrollHeight
 })
+
+fetchChat.on("child_removed", function (snapshot) { deleteMessage(snapshot.key) })
+
+function deleteMessage(id) {
+    db.ref(`messages/${id}`).remove().then((a) => console.log(a)).catch(err => console.log(err.message));
+    document.querySelector(`#${id}`).parentElement.remove();
+}
 
 const input = document.querySelector("input");
 
@@ -33,17 +51,18 @@ const sendMessage = (e) => {
     e.preventDefault();
     console.log("Send!")
     
-    const timestamp = Date.now();
     const message = input.value;
     input.value = "";
-
-    db.ref("messages/" + timestamp).set({
+    
+    db.ref("messages/").push({
         usr: username,
         msg: message,
     });
 }
 
-document.querySelector("button").addEventListener("click", (e) =>{ sendMessage(e); })
+
+
+document.querySelector("#send").addEventListener("click", (e) =>{ sendMessage(e); })
 
 document.querySelector("input").addEventListener("keypress", (e) => {
     if (e.key === "Enter") {
