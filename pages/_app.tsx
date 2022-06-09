b5f613d3ac3fe40567043accf839e34092e079af
abehidek <hidek.abe@outlook.com>
2022-06-09 10:19:54 -0300

    Add react-query lib

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { QueryClient, QueryClientProvider, useQuery } from 'react-query'
+ 
+const queryClient = new QueryClient()
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+    </QueryClientProvider>
+  );
 }
 
 export default MyApp
