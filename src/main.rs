b2b8b44198848609b0b1f886dda0168a09d94beb
abehidek <hidek.abe@outlook.com>
2022-06-24 17:58:26 -0300

    Add tuples content

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,14 @@
 // mod psrint;
 // mod vars;
 // mod types;
-mod strings;
+// mod strings;
+mod tuples;
 
 fn main() {
   // println!("Hello, world!");
   // print::run();
   // vars::run();
   // types::run();
-  strings::run();
+  // strings::run();
+  tuples::run();
 }
---
+++ src/tuples.rs
@@ -0,0 +1,9 @@
+// tuples group together values of different types
+// max 12 elements
+
+pub fn run() {
+    println!("Hello from tuples.rs!");
+
+    let person: (&str, &str, i8) = ( "abe", "japan", 20 );
+    println!("{name} is from {country} and is {age}", name=person.0, country=person.1, age=person.2)
+}
