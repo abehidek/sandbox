7b3599fcc556a1763036465b31f2b1a6578bf028
abehidek <hidek.abe@outlook.com>
2022-06-30 10:02:06 -0300

    Start refactoring

---
+++ new/pages/index.tsx
@@ -0,0 +1,14 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.scss'
+
+const Home: NextPage = () => {
+  return (
+    <div className={styles.container}>
+      <h1>Hello, World</h1>
+    </div>
+  )
+}
+
+export default Home
