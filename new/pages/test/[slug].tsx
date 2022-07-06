55064c261549b460ae74480ff0469a886c420054
abehidek <hidek.abe@outlook.com>
2022-07-06 20:26:36 -0300

    Add refactored post page with error handling

---
+++ new/pages/test/[slug].tsx
@@ -0,0 +1,76 @@
+import type { NextPage } from "next";
+import fetchRepositoryPost, {
+  FetchError,
+  Post,
+} from "../../lib/fetchRepositoryPost";
+import fetchRepositoryPosts, {
+  isFetchError,
+} from "../../lib/fetchRepositoryPosts";
+
+interface PathRoute {
+  params: {
+    slug: string;
+  };
+}
+type Prop = {
+  post: Post | FetchError;
+};
+
+interface Props {
+  props: Prop;
+}
+
+const TestPost: NextPage<Prop> = ({ post }) => {
+  console.log(post);
+  if (isFetchError(post)) {
+    return <div>Error fetching data</div>;
+  }
+  return (
+    <div className="text-white flex flex-col gap-5">
+      <header className="bg-slate-800 rounded p-2 h-48 flex flex-col">
+        <p>{post.frontmatter.title}</p>
+        <p>{post.frontmatter.date}</p>
+        <p>{post.frontmatter.excerpt}</p>
+        <img
+          src={post.frontmatter.cover_image}
+          alt={post.frontmatter.title}
+          className="h-full object-contain self-start"
+        />
+      </header>
+
+      <p>{post.content}</p>
+    </div>
+  );
+};
+
+export async function getStaticPaths() {
+  const paths = await fetchRepositoryPosts();
+  if (isFetchError(paths)) {
+    return {
+      paths: undefined,
+      fallback: false,
+    };
+  } else {
+    return {
+      paths: paths.map((path) => ({
+        params: {
+          slug: path,
+        },
+      })),
+      fallback: false,
+    };
+  }
+}
+
+export async function getStaticProps({
+  params: { slug },
+}: PathRoute): Promise<Props> {
+  const post: Post | FetchError = await fetchRepositoryPost(slug);
+  return {
+    props: {
+      post,
+    },
+  };
+}
+
+export default TestPost;
