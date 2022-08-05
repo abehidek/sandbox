1b4bc577963fa1894698486bdb77364c64ec9a96
abehidek <hidek.abe@outlook.com>
2022-08-05 16:57:54 -0300

    Move css, js, imgs and fonts files to static folder, delete scss

--- bosch/ets/ets/settings.py
+++ bosch/ets/ets/settings.py
@@ -122,7 +122,7 @@ STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'ets/static')
+    os.path.join(BASE_DIR, 'cursos/static')
 ]
 
 # Default primary key field type
