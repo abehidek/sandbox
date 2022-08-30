36a92f3cb2c813e94ea61bafaee5376c88a6d5d6
abehidek <hidek.abe@outlook.com>
2022-08-30 16:56:24 -0300

    Finish ArticlePage

--- src/pages/blog/articles/[slug].tsx
+++ src/pages/blog/articles/[slug].tsx
@@ -1,12 +1,52 @@
+import type { GetStaticProps, GetStaticPaths } from "next";
+import Image from "next/image";
+import Head from "next/head";
+import { MDXRemote, MDXRemoteSerializeResult } from "next-mdx-remote";
+import { serialize } from "next-mdx-remote/serialize";
+import rehypeSlug from "rehype-slug";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
+import rehypeHighlight from "rehype-highlight";
+import { getArticleFromSlug, getSlugs, ArticleMeta } from "@/src/server/static";
+import "highlight.js/styles/atom-one-dark.css";
 import type { NextPage } from "next";
 import { useState } from "react";
 
-const ArticlePage: NextPage = () => {
+interface MDXArticle {
+  source: MDXRemoteSerializeResult<Record<string, unknown>>;
+  meta: ArticleMeta;
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const paths = getSlugs().map((slug) => ({ params: { slug } }));
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const { slug } = params as { slug: string };
+  const { content, meta } = getArticleFromSlug(slug);
+  const mdxSource = await serialize(content, {
+    mdxOptions: {
+      rehypePlugins: [
+        rehypeSlug,
+        [rehypeAutolinkHeadings, { behavior: "wrap" }],
+        rehypeHighlight,
+      ],
+    },
+  });
+  return { props: { article: { source: mdxSource, meta } } };
+};
+
+const ArticlePage: NextPage<{ article: MDXArticle }> = ({ article }) => {
   return (
     <>
-      <main>
-        <h1>Article Page</h1>
-      </main>
+      <Head>
+        <title>{article.meta.title}</title>
+      </Head>
+      <h1>{article.meta.title}</h1>
+      <MDXRemote {...article.source} components={{ Image }} />
     </>
   );
 };
