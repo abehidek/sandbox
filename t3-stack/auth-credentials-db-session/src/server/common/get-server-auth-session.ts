88e5e9e0e447d1fb9f8be13e4e5ccbcc7fc83873
abehidek <hidek.abe@outlook.com>
2022-09-29 11:05:02 -0300

    fix: req-res types

--- t3-stack/auth-credentials-db-session/src/server/common/get-server-auth-session.ts
+++ t3-stack/auth-credentials-db-session/src/server/common/get-server-auth-session.ts
@@ -1,13 +1,15 @@
 // Wrapper for unstable_getServerSession https://next-auth.js.org/configuration/nextjs
 
-import type { GetServerSidePropsContext } from "next";
+import type { GetServerSidePropsContext, NextApiRequest, NextApiResponse } from "next";
 import { unstable_getServerSession } from "next-auth";
 import { wrapper } from "../../pages/api/auth/[...nextauth]";
 
 // Next API route example - /pages/api/restricted.ts
 export const getServerAuthSession = async (ctx: {
-  req: GetServerSidePropsContext["req"];
-  res: GetServerSidePropsContext["res"];
+  // req: GetServerSidePropsContext["req"];
+  // res: GetServerSidePropsContext["res"];
+  req: NextApiRequest;
+  res: NextApiResponse;
 }) => {
   return await unstable_getServerSession(...wrapper(ctx.req, ctx.res));
 };
