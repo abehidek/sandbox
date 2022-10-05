8446c8ca9526260fda61a1177ee1e716621498fd
abehidek <hidek.abe@outlook.com>
2022-10-05 10:17:33 -0300

    rm: prisma query on deleted example table

--- src/pages/api/examples.ts
+++ src/pages/api/examples.ts
@@ -3,7 +3,6 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import { prisma } from "../../server/db/client";
 
 const examples = async (req: NextApiRequest, res: NextApiResponse) => {
-  const examples = await prisma.example.findMany();
   res.status(200).json(examples);
 };
 
