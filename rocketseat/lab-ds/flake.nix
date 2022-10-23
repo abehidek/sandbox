0f0b0a8c45e0afec539206a28f728d8e9fda3c9c
abehidek <hidek.abe@outlook.com>
2022-10-22 22:02:48 -0300

    add: nix dev env

---
+++ rocketseat/lab-ds/flake.nix
@@ -0,0 +1,18 @@
+{
+  description = "A prisma test project";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/master";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShell = pkgs.mkShell {
+        nativeBuildInputs = [ pkgs.bashInteractive ];
+        buildInputs = with pkgs; [
+          nodejs-16_x
+          (yarn.override { nodejs = nodejs-16_x;  })
+        ];
+      };
+    });
+}
