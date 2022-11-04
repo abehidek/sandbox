8683daf2a69e8c92d00084ccefe79e8c3a4ebb91
abehidek <hidek.abe@outlook.com>
2022-11-04 19:43:59 -0300

    setup: prisma and create pools count route

--- rocketseat/nlw-copa/server/src/server.ts
+++ rocketseat/nlw-copa/server/src/server.ts
@@ -1,11 +1,19 @@
 import Fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient({
+  log: ['query'],
+})
 
 async function bootstrap() {
   const fastify = Fastify({
     logger: true,
   })
   
-  // fastify.get('')
+  fastify.get('/pools/count', async () => {
+    const count = await prisma.pool.count()
+    return { count }
+  })
     
   await fastify.listen({ port: 3333 })
 }
