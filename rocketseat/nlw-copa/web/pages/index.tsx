4e2d30f3ea3aae37ae74d08cf85aec0f10627ea0
abehidek <hidek.abe@outlook.com>
2022-11-04 20:48:52 -0300

    add: basic data fetching inside nextjs ssp

--- rocketseat/nlw-copa/web/pages/index.tsx
+++ rocketseat/nlw-copa/web/pages/index.tsx
@@ -1,7 +1,22 @@
-export default function Home() {
+interface Props {
+  count: number
+}
+
+export default function Home(props: Props) {
   return (
     <div>
       <h1>Hello World</h1>
+      <p>Count: {props.count}</p>
     </div>
   )
+}
+
+export const getServerSideProps = async () => {
+  const response = await (await fetch('http://localhost:3333/pools/count')).json()
+
+  return {
+    props: {
+      count: response.count
+    }
+  }
 }
