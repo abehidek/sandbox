6c827ecc136a3e00fadb8cb6c0bba01fae5a7c78
abehidek <hidek.abe@outlook.com>
2022-11-04 20:26:53 -0300

    setup: fastify cors and nw related stuff

--- rocketseat/nlw-copa/server/src/server.ts
+++ rocketseat/nlw-copa/server/src/server.ts
@@ -1,4 +1,5 @@
 import Fastify from 'fastify'
+import fastifyCors from '@fastify/cors'
 import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient({
@@ -9,13 +10,15 @@ async function bootstrap() {
   const fastify = Fastify({
     logger: true,
   })
+
+  await fastify.register(fastifyCors, { origin: true })
   
   fastify.get('/pools/count', async () => {
     const count = await prisma.pool.count()
     return { count }
   })
     
-  await fastify.listen({ port: 3333 })
+  await fastify.listen({ port: 3333, host: '0.0.0.0' })
 }
 
 bootstrap()
