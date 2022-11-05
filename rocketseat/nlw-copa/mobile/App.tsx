2db6835a4f5b9d0ffdc5e1c3df203705ff57748f
abehidek <hidek.abe@outlook.com>
2022-11-04 22:42:54 -0300

    setup: react-query to react-native

--- rocketseat/nlw-copa/mobile/App.tsx
+++ rocketseat/nlw-copa/mobile/App.tsx
@@ -1,20 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import HomeScreen from './screens/Home';
+
+const queryClient = new QueryClient()
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <QueryClientProvider client={queryClient}>
+      <HomeScreen />
+    </QueryClientProvider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ rocketseat/nlw-copa/mobile/screens/Home.tsx
@@ -0,0 +1,30 @@
+import { useQuery } from '@tanstack/react-query';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function HomeScreen() {
+  const query = useQuery(["todos"], () =>
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+      .then(response => response.json())
+  )
+
+  if (query.isLoading) return <Text>Loading...</Text>
+  if (query.error) return <Text>{JSON.stringify(query.error)}</Text>
+
+  return (
+    <View style={styles.container}>
+      <Text>Home!!</Text>
+      <Text>{JSON.stringify(query.data)}</Text>
+      <StatusBar style="auto" />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
