c699962cd0478c40289313cbcb1cf2844e30ba7d
abehidek <hidek.abe@outlook.com>
2022-11-04 23:01:01 -0300

    config: native base component lib in rn app

--- rocketseat/nlw-copa/mobile/App.tsx
+++ rocketseat/nlw-copa/mobile/App.tsx
@@ -1,4 +1,5 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { NativeBaseProvider } from 'native-base';
 import HomeScreen from './src/screens/Home';
 
 const queryClient = new QueryClient()
@@ -6,7 +7,9 @@ const queryClient = new QueryClient()
 export default function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <HomeScreen />
+      <NativeBaseProvider>
+        <HomeScreen />
+      </NativeBaseProvider>
     </QueryClientProvider>
   );
 }
