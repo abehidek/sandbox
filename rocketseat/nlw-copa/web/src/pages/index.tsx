f3fb39af1a8183669fafd7855577cce7d0bcfdec
abehidek <hidek.abe@outlook.com>
2022-11-06 16:52:10 -0300

    web: finish home page design from figma

--- rocketseat/nlw-copa/web/src/pages/index.tsx
+++ rocketseat/nlw-copa/web/src/pages/index.tsx
@@ -1,12 +1,71 @@
+import appPreviewImg from '../assets/app-nlw-copa-preview.png'
+import logoImg from '../assets/logo.svg'
+import usersAvatarExampleImg from '../assets/users-avatar-example.png'
+import iconCheckImg from '../assets/icon-check.svg'
+
+import Image from 'next/image'
+
 interface Props {
   count: number
 }
 
 export default function Home(props: Props) {
   return (
-    <div>
-      <h1>Hello World</h1>
-      <p>Count: {props.count}</p>
+    <div className='max-w-[1124px] h-screen mx-auto grid grid-cols-2 items-center gap-28'>
+      <main>
+        <Image src={logoImg} alt="NLW Copa" />
+
+        <h1 className='mt-14 text-white text-5xl leading-tight font-bold'>Crie seu próprio bolão da copa e compartilhe entre amigos!</h1>
+
+        <div className='mt-10 flex items-center gap-2'>
+          <Image src={usersAvatarExampleImg} alt="" />
+
+          <strong className='text-gray-100 text-xl'>
+            <span className='text-ignite-500'>+12.592</span> pessoas já estão usando
+          </strong>
+        </div>
+
+        <form className='mt-10 flex gap-2'>
+          <input
+            className='flex-1 px-6 py-4 rounded bg-gray-800 border border-gray-600 text-sm'
+            type="text"
+            required
+            placeholder='Qual nome do seu bolão?'
+          />
+          <button
+            className='bg-yellow-500 px-6 py-4 rounded text-gray-900 font-bold uppercase text-sm hover:bg-yellow-700'
+            type="submit"
+          >
+            Criar meu bolão
+          </button>
+        </form>
+
+        <p className='mt-4 text-sm text-gray-300 leading-relaxed'>Após criar seu bolão, você receberá um código único que poderá usar para convidar outras pessoas 🚀</p>
+
+        <div className='mt-10 pt-10 border-t border-gray-600 items-center flex justify-between text-gray-100'>
+          <div className='flex items-center gap-6'>
+            <Image src={iconCheckImg} alt="" />
+            <div className='flex flex-col'>
+              <span className='font-bold text-2xl'>+2.034</span>
+              <span>Bolões criados</span>
+            </div>
+          </div>
+          <div className='w-px h-14 bg-gray-600'/>
+          <div className='flex items-center gap-6'>
+            <Image src={iconCheckImg} alt="" />
+            <div className='flex flex-col'>
+              <span className='font-bold text-2xl'>+192.847</span>
+              <span>Palpites enviados</span>
+            </div>
+          </div>
+        </div>
+
+      </main>
+      <Image
+        src={appPreviewImg}
+        alt="Dois celulares exibindo uma prévia da aplicação móvel do NLW Copa"
+        quality={100}
+      />
     </div>
   )
 }
--- rocketseat/nlw-copa/web/tailwind.config.js
+++ rocketseat/nlw-copa/web/tailwind.config.js
@@ -4,7 +4,33 @@ module.exports = {
     "./src/**/*.tsx"
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: 'Roboto, sans-serif'
+      },
+
+      backgroundImage: {
+        app: 'url(/app-bg.png)'
+      },
+
+      colors: {
+        ignite: {
+          500: '#129E57'
+        },
+
+        yellow: {
+          500: '#F7DD43',
+          700: '#E5CD3D'
+        },
+        gray: {
+          100: '#E1E1E6',
+          300: '#8D8D99',
+          600: '#323238',
+          800: '#202024',
+          900: '#121214'
+        }
+      }
+    },
   },
   plugins: [],
 }
