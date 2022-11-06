26d4dbacdbc2c5f87472846affb818283b6dc4d5
abehidek <hidek.abe@outlook.com>
2022-11-06 18:33:16 -0300

    web: switch SSR -> ISR to all queries in home page

--- rocketseat/nlw-copa/web/src/pages/index.tsx
+++ rocketseat/nlw-copa/web/src/pages/index.tsx
@@ -6,6 +6,7 @@ import iconCheckImg from '../assets/icon-check.svg'
 import Image from 'next/image'
 import { api } from '../lib/axios'
 import { FormEvent, useState } from 'react'
+import { GetStaticProps } from 'next'
 
 interface Props {
   poolCount: number
@@ -99,7 +100,7 @@ export default function Home(props: Props) {
   )
 }
 
-export const getServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const [poolCountResponse, guessCountResponse, userCountResponse] = await Promise.all([
     api.get("pools/count"),
     api.get("guesses/count"),
@@ -111,6 +112,7 @@ export const getServerSideProps = async () => {
       poolCount: poolCountResponse.data.count,
       guessCount: guessCountResponse.data.count,
       userCount: userCountResponse.data.count
-    }
+    },
+    revalidate: 10,
   }
 }
