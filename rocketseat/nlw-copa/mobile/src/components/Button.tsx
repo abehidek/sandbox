41aa3c95aacaf18d61ea04158072693a571b3ea5
abehidek <hidek.abe@outlook.com>
2022-11-06 19:19:22 -0300

    mobile: finish SignIn page and add button component

---
+++ rocketseat/nlw-copa/mobile/src/components/Button.tsx
@@ -0,0 +1,35 @@
+import { Button as ButtonNativeBase, Text, IButtonProps } from "native-base"
+
+interface Props extends IButtonProps{
+  title: string;
+  type?: 'PRIMARY' | 'SECONDARY'
+}
+
+export function Button({ title, type="PRIMARY", ...rest }: Props) {
+  return (
+    <ButtonNativeBase 
+      w="full"
+      h={14}
+      rounded="sm"
+      fontSize="md"
+      textTransform="uppercase"
+      bg={type === 'SECONDARY' ? 'red.500' : 'yellow.500'}
+      _pressed={{
+        bg: type === 'SECONDARY' ? 'red.600' : 'yellow.600'
+      }}
+
+      _loading={{
+        _spinner: { color: 'black' }
+      }}
+      {...rest}
+    >
+      <Text
+        fontSize="sm"
+        fontFamily="heading"
+        color={type === 'SECONDARY' ? 'white' : 'black'}
+      >
+        {title}
+      </Text>
+    </ButtonNativeBase>
+  )
+}
--- rocketseat/nlw-copa/mobile/src/screens/SignIn.tsx
+++ rocketseat/nlw-copa/mobile/src/screens/SignIn.tsx
@@ -1,12 +1,24 @@
-import { Center, Text } from "native-base";
+import { Center, Text, Icon } from "native-base";
+import { Fontisto } from '@expo/vector-icons'
 
 import Logo from '../assets/logo.svg';
+import { Button } from "../components/Button";
 
 export default function SignInScreen() {
- return (
-  <Center flex={1} bgColor="gray.900">
-    <Text>Sign In</Text>
-    <Logo width={212} height={40} />
-  </Center>
- )
+  return (
+    <Center flex={1} bgColor="gray.900" p={7}>
+      <Text>Sign In</Text>
+      <Logo width={212} height={40} />
+      <Button
+        title="ENTRAR COM GOOGLE"
+        type="SECONDARY"
+        leftIcon={<Icon as={Fontisto} name="google" color="white" size="md" />}
+        mt={12}
+      />
+
+      <Text color="white" textAlign='center' mt={4}>
+        Não utilizamos nenhuma informação além {'\n'} do seu e-mail para criação de sua conta.
+      </Text>
+    </Center>
+  )
 }
