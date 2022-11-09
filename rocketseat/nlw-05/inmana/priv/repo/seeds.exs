db842b85789da79cf624dc7ec5948bac2c7bb433
abehidek <hidek.abe@outlook.com>
2022-11-08 23:46:46 -0300

    nlw-05-elixir: create db data seed

--- rocketseat/nlw-05/inmana/priv/repo/seeds.exs
+++ rocketseat/nlw-05/inmana/priv/repo/seeds.exs
@@ -9,3 +9,59 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+# Creating restaurants
+
+Inmana.Repo.delete_all(Inmana.Supply)
+
+Inmana.Repo.delete_all(Inmana.Restaurant)
+
+{:ok, mcdonalds} = %{name: "McDonalds", email: "mc@donalds.com"} |> Inmana.create_restaurant()
+
+%{
+  restaurant_id: mcdonalds.id,
+  description: "Carne moída",
+  expiration_date: "2022-11-02",
+  responsible: "Pedro"
+}
+|> Inmana.create_supply()
+
+%{
+  restaurant_id: mcdonalds.id,
+  description: "Maionese",
+  expiration_date: "2022-11-04",
+  responsible: "Amanda"
+}
+|> Inmana.create_supply()
+
+%{
+  restaurant_id: mcdonalds.id,
+  description: "Ketchup",
+  expiration_date: "2022-11-08",
+  responsible: "Amanda"
+}
+|> Inmana.create_supply()
+
+%{
+  restaurant_id: mcdonalds.id,
+  description: "Mostarda",
+  expiration_date: "2022-11-09",
+  responsible: "Vitor"
+}
+|> Inmana.create_supply()
+
+%{
+  restaurant_id: mcdonalds.id,
+  description: "Barbecue",
+  expiration_date: "2022-11-13",
+  responsible: "Cléber"
+}
+|> Inmana.create_supply()
+
+%{
+  restaurant_id: mcdonalds.id,
+  description: "Molho ingles",
+  expiration_date: "2022-11-15",
+  responsible: "Daniel"
+}
+|> Inmana.create_supply()
