6b628847cef0a9301e09bbdd4cc1ff147149f74e
abehidek <hidek.abe@outlook.com>
2022-11-20 21:01:25 -0300

    nlw-copa: improve Participants component data display

--- rocketseat/nlw-copa/mobile/src/components/Participants.tsx
+++ rocketseat/nlw-copa/mobile/src/components/Participants.tsx
@@ -13,31 +13,36 @@ interface Props {
   count: number;
 };
 
-export function Participants({participants, count }: Props) {
+export function Participants({ participants, count }: Props) {
   return (
     <HStack>
       {
         participants && participants.map((participant) => (
           <Avatar
             key={participant.id}
-            source={{ uri: participant.user.avatarUrl}}
+            source={{ uri: participant.user.avatarUrl }}
             w={8}
             h={8}
             rounded="full"
             borderWidth={2}
             marginRight={-3}
             borderColor="gray.800"
-          >
-           {participant.user?.name?.at(0).toUpperCase()}
-          </Avatar>
+          />
         ))
       }
 
-      <Center w={8} h={8} bgColor="gray.700" rounded="full" borderWidth={1} borderColor="gray.800">
-        <Text color="gray.100" fontSize="xs" fontFamily="medium">
-          {count ? `+${count}` : 0}
-        </Text>
-      </Center>
+      {
+        count && participants.length - count !== 0
+          ?
+          <Center w={8} h={8} bgColor="gray.700" rounded="full" borderWidth={1} borderColor="gray.800">
+            <Text color="gray.100" fontSize="xs" fontFamily="medium">
+              +{participants.length - count}
+            </Text>
+          </Center>
+          : null
+      }
+
+
     </HStack>
   );
 }
