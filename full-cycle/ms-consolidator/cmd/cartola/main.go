f965a13925679e593d306a33a502157eba4d18c7
abehidek <hidek.abe@outlook.com>
2022-12-17 21:53:22 -0300

    feat(fc/consolidator): finish routing httphandler fns

--- full-cycle/ms-consolidator/cmd/cartola/main.go
+++ full-cycle/ms-consolidator/cmd/cartola/main.go
@@ -3,15 +3,26 @@ package main
 import (
 	"context"
 	"database/sql"
+	"net/http"
 
 	"github.com/abehidek/sandbox/internal/infra/db"
+	httphandler "github.com/abehidek/sandbox/internal/infra/http"
+
+	// "github.com/abehidek/sandbox/internal/infra/kafka/consumer"
 	"github.com/abehidek/sandbox/internal/infra/repository"
 	"github.com/abehidek/sandbox/pkg/uow"
+
+	// "github.com/confluentinc/confluent-kafka-go/kafka"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/cors"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
 	ctx := context.Background()
-	dtb, err := sql.Open("mysql", "root:root@tcp(localhost:3306)/cartola?parseTime=true")
+	dtb, err := sql.Open("mysql", "root:root@tcp(mysql:3306)/cartola?parseTime=true")
+
 	if err != nil {
 		panic(err)
 	}
@@ -21,6 +32,30 @@ func main() {
 		panic(err)
 	}
 	registerRepositories(uow)
+
+	r := chi.NewRouter()
+	r.Use(cors.Handler(cors.Options{
+		// AllowedOrigins:   []string{"https://foo.com"}, // Use this to allow specific origin hosts
+		AllowedOrigins: []string{"https://*", "http://*"},
+		// AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300, // Maximum value not ignored by any of major browsers
+	}))
+	r.Get("/players", httphandler.ListPlayersHandler(ctx, *db.New(dtb)))
+	r.Get("/my-teams/{teamID}/players", httphandler.ListMyTeamPlayersHandler(ctx, *db.New(dtb)))
+	r.Get("/my-teams/{teamID}/balance", httphandler.GetMyTeamBalanceHandler(ctx, *db.New(dtb)))
+	r.Get("/matches", httphandler.ListMatchesHandler(ctx, repository.NewMatchRepository(dtb)))
+	r.Get("/matches/{matchID}", httphandler.ListMatchByIDHandler(ctx, repository.NewMatchRepository(dtb)))
+
+	go http.ListenAndServe(":8080", r)
+
+	// var topics = []string{"newMatch", "chooseTeam", "newPlayer", "matchUpdateResult", "newAction"}
+	// msgChan := make(chan *kafka.Message)
+	// go consumer.Consume(topics, "host.docker.internal:9094", msgChan)
+	// consumer.ProcessEvents(ctx, msgChan, uow)
 }
 
 func registerRepositories(uow *uow.Uow) {
--- full-cycle/ms-consolidator/go.mod
+++ full-cycle/ms-consolidator/go.mod
@@ -2,4 +2,10 @@ module github.com/abehidek/sandbox
 
 go 1.19
 
-require github.com/google/uuid v1.3.0
+require (
+	github.com/confluentinc/confluent-kafka-go v1.9.2
+	github.com/go-chi/chi/v5 v5.0.8
+	github.com/go-chi/cors v1.2.1
+	github.com/go-sql-driver/mysql v1.7.0
+	github.com/google/uuid v1.3.0
+)
