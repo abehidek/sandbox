95fd377a8fa40837542c02da29af6c571854af6d
abehidek <hidek.abe@outlook.com>
2023-05-12 08:44:16 -0300

    feat(elixir-graphql): add todo_item queries and mutations
    - object :todo_item
    - mutations:
      - create_todo_item
      - toggle_todo_item
    - queries:
      - todo_items

--- elixir-graphql/server/lib/server_web/schema.ex
+++ elixir-graphql/server/lib/server_web/schema.ex
@@ -1,9 +1,48 @@
 defmodule ServerWeb.Schema do
+  alias Server.Todos
   use Absinthe.Schema
+
+  object :todo_item do
+    field(:id, non_null(:id))
+    field(:content, non_null(:string))
+
+    field :is_completed, non_null(:boolean) do
+      resolve(fn %{completed_at: completed_at}, _, _ ->
+        {:ok, !is_nil(completed_at)}
+      end)
+    end
+  end
+
+  mutation do
+    field :create_todo_item, non_null(:boolean) do
+      arg(:content, non_null(:string))
+
+      resolve(fn %{content: content}, _ ->
+        case Todos.create_todo(%{content: content}) do
+          {:ok, %Todos.Todo{}} -> {:ok, true}
+          _ -> {:ok, false}
+        end
+      end)
+    end
+
+    field :toggle_todo_item, :todo_item do
+      arg(:id, non_null(:id))
+
+      resolve(fn %{id: todo_id}, _ ->
+        Todos.toggle_todo_by_id(todo_id)
+      end)
+    end
+  end
 
   query do
     field :hello, :string do
-      resolve(fn _, _ -> {:ok, "Hello World"}end)
+      resolve(fn _, _ -> {:ok, "Hello World"} end)
+    end
+
+    field :todo_items, non_null(list_of(:todo_item)) do
+      resolve(fn _, _ ->
+        {:ok, Todos.list_todos()}
+      end)
     end
   end
 end
